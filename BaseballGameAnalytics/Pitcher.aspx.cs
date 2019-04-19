058e27e6140827c36f3955823e50fbf5fcb6997f
Alec Ostheimer <alecostheimer94@gmail.com>
2019-04-18 18:07:21 -0600

    helasdf

--- BaseballGameAnalytics/Pitcher.aspx.cs
+++ BaseballGameAnalytics/Pitcher.aspx.cs
@@ -31,5 +31,10 @@ namespace BaseballGameAnalytics
             Players.DataBind();
             connection.Close();
         }
+
+        protected void pitcherButton_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
